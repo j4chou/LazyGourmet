0df7d0a140b6e6b3f8b792d49fefb438e97de171
Jessica Chou <j4chou@gmail.com>
2016-04-04 21:11:27 -0700

    Working on YT API
    Need to embed

--- client/app.js
+++ client/app.js
@@ -12,7 +12,13 @@ var app = {
     app.searchRecipes(input);
   },
 
+  // displayRecipe: function(item) {
+  //   item.recipes.forEach(function(recipe) {
+  //     console.log('recipe.title is', recipe.title);
+  //   })
+  // },
   searchRecipes: function(input) {
+   $('.recipeBlock').empty();
 
     $.ajax({
       type: 'GET',
@@ -20,8 +26,29 @@ var app = {
       'content-type':'application/json',
       data: input,
       success: function(data) {
-        console.log("Inside search recipes - success: data is", JSON.parse(data.body));
-        
+        var list = JSON.parse(data.body);
+        list.count = 10;
+        list.recipes.length = 10;
+        console.log('list is', list);
+
+        list.recipes.forEach(function(recipe) {
+          console.log('recipe.title is', recipe.title);
+          console.log('image url is', recipe.image_url);
+
+          // $('.recipeBlock').append(
+          //     '<span>' + recipe.title + '</span>' + '<br>' +
+          //     '<img class="recipePics" align="left" src="'+ recipe.image_url +'"/><br>'
+          //   );
+
+        $('.recipeBlock').append(
+            '<div class="post-container">' +
+    '<h3 class="post-title">' + recipe.title + '</h3>' +
+    '<div class="post-thumb">' +
+    '<img class ="recipePics" src=" ' + recipe.image_url + '"/> ' + '</div>' +
+    '<div class="post-content">' +
+        '<p>' + '</p>' + '</div>' + '</div>'
+         );
+        });
 
       },
       error: function() {
@@ -29,5 +56,22 @@ var app = {
       }
     });
   }
+
+  // retrieveVideos: function(input) {
+  //   $.ajax({
+  //     type: 'POST',
+  //     url: 'http://127.0.0.1:3000',
+  //     'content-type':'application/json',
+  //     data: input,
+  //     success: function(data) {
+  //       console.log('data inside retrieveVideos is', data);
+
+  //     },
+  //     error: function() {
+  //       console.log("Invalid query parameters");
+  //     }
+  //   });
+  // }
+
   
 };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
-var app = express();
+
+var search = require('youtube-search');
+var opts = {
+  maxResults: 2,
+  key: 'AIzaSyAH0GMnEtMRm6g8A1kDNMaiuzpGxYl9-QM'
+};
 
 app.use(bodyParser.json());
 
@@ -15,20 +21,52 @@ var apiKey = '0bf4be4ce57a50dfc87815612972e33a';
 
 var url = 'http://food2fork.com/api/search?key=' + apiKey + '&q=' ;
 
-app.get('/', function(req, res) {
-	console.log("inside server.js app.get");
-	console.log("req.query is", Object.keys(req.query));
-	var param = Object.keys(req.query)[0].split(" ").join('%20');
+        // youTube search 
 
-	var link = url + param;
-	request(''+link, function (error, body) {
+function searchYoutube(input) {
+  var recipe = input + '%20recipe';
+  console.log('recipe is', recipe);
+  return search(recipe, opts, function(err, results) {
+    if (err) return console.log(err);
+    console.log("inside search youtube");
+    console.log('results are',results);
+
+    results;
+    });
+};
+
+
+//.replace(/\s+\,+/g, '%20')
+
+app.get('/', function(req, res) {
+
+  console.log("inside server.js app.get");
+  console.log("req.query is", Object.keys(req.query));
+  var param = Object.keys(req.query)[0];
+  console.log("param is", param);
+
+  var videoResults = searchYoutube(param);
+  console.log("video results inside app.get are",videoResults);
+
+  var link = url + param;
+
+  request(link, function (error, body) {
     if (!error && res.statusCode == 200) {
-      console.log("body is:",body);
+      var dataArray = [body, videoResults];
+      console.log("DATA ARRAY IS", dataArray);
       res.send(body); 
     }
   });
+  
+
 })
 
+
+
+// app.post('/', function(req, res) {
+//   console.log("testing post");
+//   searchYoutube(param, res);
+// });
 
 
 var port = 3000;
